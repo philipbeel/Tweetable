b3d7c3fa2c1b56f2b1cbe6acfee071fc5d6b7778
philipbeel <philipbeel@googlemail.com>
2013-06-16 19:43:24 +0100

    Modified: new api endpoint added for version 1.1

--- tweetable.jquery.js
+++ tweetable.jquery.js
@@ -1,14 +1,13 @@
 /*
- * tweetable 1.7.0 - jQuery twitter feed plugin
+ * tweetable 1.7.1 - jQuery twitter feed plugin
  *
  * Copyright (c) 2009 Philip Beel (http://www.theodin.co.uk/)
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * With modifications from Philipp Robbel (http://www.robbel.com/) and Patrick DW (stackoverflow)
- * for IE compatibility.
+ * With modifications from Philipp Robbel (http://www.robbel.com/) & Patrick DW (stackoverflow)
  *
- * Revision: $Id: jquery.tweetable.js 2012-07-08 $ 
+ * Revision: $Id: jquery.tweetable.js 2013-06-16 $ 
  *
  */
 (function($) {
@@ -16,16 +15,13 @@
 	jQuery.fn.tweetable = function (opts) {
 		opts = $.extend({}, $.fn.tweetable.options, opts);
 
-		// Loop through each instance
 		return this.each(function () {
 
 			var act = jQuery(this)
 			,   tweetList = jQuery('<ul class="tweetList">')[opts.position.toLowerCase() + 'To'](act)
 			,   shortMonths = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-			,   api = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=false&suppress_response_codes=true&screen_name="
-			,   count = "&count="
-			,   replies = "&exclude_replies="
-			,   rts = "&include_rts="
+			,	 api = "http://api.getmytweets.co.uk/?screenname="
+			,   limitcount = "&limit="
 			,   twitterError
 			,   tweetMonth
 			,   tweetMonthInt
@@ -33,39 +29,39 @@
 			,   element;
 
 			// Fire JSON request to twitter API
-			jQuery.getJSON(api + opts.username + count + (opts.limit + 5) + replies + opts.replies + rts + opts.retweets + "&callback=?", act, function (data) {
+			jQuery.getJSON(api + opts.username + limitcount + opts.limit, act, function (data) {
 
 				// Check for response error 
 				twitterError = data && data.error || null;
 
 				if(twitterError)
 				{
-					tweetList.append('<li class="tweet_content"><p class="tweet_link">'+ opts.failed +'</p></li>');
+					tweetList.append('<li class="tweet_content item"><p class="tweet_link">'+ opts.failed +'</p></li>');
 					return;
 				}
 
 				// Loop through twitter API response
-				jQuery.each(data, function (i, tweet) {
+				jQuery.each(data.tweets, function (i, tweet) {
 
 					// Output tweets if less than limit
 					if(i >= opts.limit)
 						return;
 
-					tweetList.append('<li class="tweet_content_' + i + '"><p class="tweet_link_' + i + '">' + tweet.text.replace(/#(.*?)(\s|$)/g, '<span class="hash">#$1 </span>').replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, '<a href="$&">$&</a> ').replace(/@(.*?)(\s|\(|\)|$)/g, '<a href="http://twitter.com/$1">@$1 </a>$2') + '</p></li>');
+					tweetList.append('<li class="tweet_content_' + i + '"><p class="tweet_link_' + i + '">' + tweet.response.replace(/#(.*?)(\s|$)/g, '<span class="hash">#$1 </span>').replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, '<a href="$&">$&</a> ').replace(/@(.*?)(\s|\(|\)|$)/g, '<a href="http://twitter.com/$1">@$1 </a>$2') + '</p></li>');
 					
 					// Display the time of tweet if required
 					if (opts.time === true) {
 						for(iterate=0; iterate<=12; iterate++) {
-							if(shortMonths[iterate] === tweet.created_at.substr(4, 3)) {
+							if(shortMonths[iterate] === tweet.tweet_date.substr(4, 3)) {
 								tweetMonthInt = iterate + 1;
 								tweetMonth = (tweetMonthInt < 10) ? '0' + tweetMonthInt : tweetMonthInt ;
 							}
 						}
 						// Create ISO 8601 formatted date
-						var iso8601 = tweet.created_at.substr(26,4) + '-' + tweetMonth + '-' + tweet.created_at.substr(8, 2) + 'T' + tweet.created_at.substr(11,8) + 'Z';  
+						var iso8601 = tweet.tweet_date.substr(26,4) + '-' + tweetMonth + '-' + tweet.tweet_date.substr(8, 2) + 'T' + tweet.tweet_date.substr(11,8) + 'Z';  
 						jQuery('.tweet_link_' + i).append('<p class="timestamp"><'
 							+ ((opts.html5) ? 'time datetime="' + iso8601 + '"' : 'small') 
-							+ '> ' + tweet.created_at.substr(8, 2) + '/' + tweetMonth + '/' + tweet.created_at.substr(26,4) + ', ' + tweet.created_at.substr(11,5) + '</' 
+							+ '> ' + tweet.tweet_date.substr(8, 2) + '/' + tweetMonth + '/' + tweet.tweet_date.substr(26,4) + ', ' + tweet.tweet_date.substr(11,5) + '</' 
 							+ ((opts.html5) ? 'time' : 'small') + 
 							'></p>');
 					}
